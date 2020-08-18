a75ff03ed263da61fdeb7b5453d675b75d9fe8c6
Mohammadnim123 <mm.nemrawi96@gmail.com>
2020-08-18 13:37:47 +0300

    edit done

--- math_series/series.py
+++ math_series/series.py
@@ -1,6 +1,32 @@
 
+def sum_series(i, p1=0, p2=1):
+  """
+  Generate ageneric serise based on aregument
+
+  Areguments :
+
+  i {integer} -- [index use to generate a serise]
+  p1 {integer} -- [to determine the base case for index 0]
+  p2 {integer} -- [to determine the base case for index 1]
+  Returns :
+  {integer}  --  [generic math serise based on p1,p2]
+
+  """
+  if i == 0: #case 0
+    return p1
+  elif i == 1: #case 1
+    return p2
+  return sum_series(i-1, p1, p2) + sum_series(i-2, p1, p2)
+
+  
+    
+
 
 def fibonacci(i):
+  """
+  it like sum serise with base value 0 for i=0 and 1 for i =1
+
+  """
   if i == 0:
     return 0
   elif i == 1:
@@ -10,6 +36,10 @@ def fibonacci(i):
 # ----------------------------------------------------------------
 
 def lucas(i):
+  """
+    it like sum serise with base value 2 for i=0 and 1 for i =1
+
+  """
   if i == 0:
     return 2
   elif i == 1:
@@ -17,12 +47,4 @@ def lucas(i):
   return lucas(i-1)+lucas(i-2) 
 
 # ------------------------------------------------------------------
-
-def sum_series(i, p1=0, p2=1):
-    
-    if i == 0:
-        return p1
-    elif i == 1:
-        return p2
-    return sum_series(i-1, p1, p2) + sum_series(i-2, p1, p2)
         
